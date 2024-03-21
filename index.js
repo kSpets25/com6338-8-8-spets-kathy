de3b43c7c3b85067718348984281f0b59e8a1097
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-03-20 20:10:34 -0400

    updated temp, icon, google map, and description for location in js file

--- index.js
+++ index.js
@@ -11,10 +11,8 @@
     //append to page.
 //create .catch function (err) to catch errors.
 
-var weatherEl = document.getElementById("weather")
 var userQuery = document.getElementById("weather-search")
 var btn = document.querySelector("button")
-
 var URL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q="
 var weatherDiv = document.getElementById('weather')
 var form = document.querySelector('form')
@@ -25,8 +23,7 @@ form.onsubmit = function(e) {
     e.preventDefault()
     var userQuery = this.search.value.trim();
     console.log("this", this)
-    //if (!userQuery) {return}
-    console.log("this",this)
+    
     const APIKey = "8907802964041b9d0da4e362e4a9789c";
     console.log (URL,userQuery,APIKey)
     fetch(URL + userQuery + "&appid=" + APIKey)
@@ -37,28 +34,54 @@ form.onsubmit = function(e) {
     console.log("res",res)
 })
     .then(function(data) {
+        weatherDiv.innerHTML = ""
+        form.search.value = ""
+        
         console.log("laterudata",data)
         console.log("main",data.main)
+        console.log("main",data.main.feels_like)
+        console.log("main",data.main.temp)
         console.log("weather",data.weather)
-        console.log("weather",data.weather[0])
         console.log("weather",data.weather[0].description)
+        console.log("weather",data.weather[0])
+        console.log("weather",data.weather[0].main)
         console.log("weather",data.weather[0].icon)
+        console.log(data.sys.country)
+
+        console.log(weather)
 
         var h2 = document.createElement('h2')
-        h2.textContent = data.name
+        h2.textContent = (data.name + ", " + data.sys.country)
         weatherDiv.appendChild(h2)
 
-        var temp = document.createElement('temp')
+        var aTag = document.createElement('a');
+        aTag.setAttribute('href', "https://www.google.com/maps/search/?api=1&query=" + data.coord.lat + "," + data.coord.lon)
+        aTag.textContent = "click to view map";
+        //window.open(aTag, "_blank")
+        weatherDiv.appendChild(aTag);
+
+        var img = document.createElement('img')
+       //img.src = "https://openweathermap.org/img/wn/10d@2x.png"
+        img.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+        console.log("image",img)
+        weatherDiv.appendChild(img);
+
+        var weatherMain = document.createElement('p')
+        weatherMain.textContent = data.weather[0].description.toUpperCase()
+        weatherDiv.appendChild(weatherMain)
+        
+        var temp = document.createElement('p')
         temp.textContent = ("Current:  " + data.main.temp + "°")
         weatherDiv.appendChild(temp)
+        
+        var temp2 = document.createElement("p")
+        temp2.textContent = ("Feels like: "  + data.main.feels_like + "°")
+        weatherDiv.appendChild(temp2)
 
-        var temp = document.createElement('temp')
-        temp.textContent = ("Feels like:  " + data.main.feels_like + "°")
-        weatherDiv.appendChild(temp)
+        
 
-        var weatherDesc = document.createElement('weather-description')
-        temp.textContent = data.weather[0].description
-        weatherDiv.appendChild(p)
+
+    
 
        
         
